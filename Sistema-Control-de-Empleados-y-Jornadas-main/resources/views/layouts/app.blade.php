759b5102434565b96997b78dbc5071de4d6f410b
Kiwop <info@kiwop.com>
2023-11-09 11:20:15 +0100

    canvi nom

--- Sistema-Control-de-Empleados-y-Jornadas-main/resources/views/layouts/app.blade.php
+++ Sistema-Control-de-Empleados-y-Jornadas-main/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Work Planner</title>
+    <title>Portal treballador Kiwop</title>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
