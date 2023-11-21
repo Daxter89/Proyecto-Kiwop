bbe82006512f9cb5f7fa1715ea4af27896cda7ed
Daxter89 <daxterxavi@gmail.com>
2023-11-21 11:53:06 +0100

    Modificació dels camps d'usuaris.
    Ja es pot modificar tots els camps dels usuaris, només en cas que siguis administrador, si ets un empleat no pots accedir a la pàgina d'edició.

--- app/Http/Controllers/GestionEmpleadosController.php
+++ app/Http/Controllers/GestionEmpleadosController.php
@@ -32,4 +32,28 @@ class GestionEmpleadosController extends Controller
         return view('admin.editar_empleado', compact('usuario'));
     }
 
+
+    public function actualizar(Request $request, User $usuario)
+    {
+    // Validación de campos
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'password' => 'nullable|string|min:8|confirmed',
+        'rol' => 'required|in:admin,employee',
+        // Agrega otras reglas de validación según sea necesario
+    ]);
+
+    // Actualiza los campos del usuario con los nuevos valores
+    $usuario->update([
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'password' => $request->input('password') ? bcrypt($request->input('password')) : $usuario->password,
+        'rol' => $request->input('rol'),
+        // Actualiza otros campos según sea necesario
+    ]);
+
+    return redirect()->route('admin.usuarios')->with('success', 'Usuario actualizado correctamente.');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -58,10 +58,6 @@ Route::middleware(['rol:admin'])->group(function () {
     Route::get('/admin/usuarios', [GestionEmpleadosController::class, 'gestion'])->name('admin.usuarios');
     Route::put('/usuarios/{usuario}/desactivar', [GestionEmpleadosController::class, 'desactivar'])->name('usuarios.desactivar');
     Route::put('/usuarios/{usuario}/reactivar', [GestionEmpleadosController::class, 'reactivar'])->name('usuarios.reactivar');
-
-    //Ruta para editar usuarios------------------------------------------------------------
-    Route::get('/usuarios/{usuario}/editar', [GestionEmpleadosController::class, 'editar'])->name('usuarios.editar');
-
     Route::get('/solicitudes-pendientes', [SolicitudesPendientesController::class, 'showPending'])->name('solicitudes-pendientes');
     Route::post('/aprobar/{solicitud}', [SolicitudesPendientesController::class, 'approve'])->name('approve');
     Route::post('/desaprobar/{solicitud}', [SolicitudesPendientesController::class, 'reject'])->name('reject');
@@ -74,6 +70,11 @@ Route::middleware(['rol:admin'])->group(function () {
 
     //Ruta para exportar CSV-----------------------------------------------------------
     Route::get('/exportar-csv', [HorarioGeneralController::class, 'exportarCSV'])->name('exportarCSV');
+
+    //Ruta para editar usuarios------------------------------------------------------------
+    Route::get('/usuarios/{usuario}/editar', [GestionEmpleadosController::class, 'editar'])->name('usuarios.editar');
+    Route::put('/usuarios/{usuario}/actualizar', [GestionEmpleadosController::class, 'actualizar'])->name('usuarios.actualizar');
+
     // Redirect para editar usuario desde el dashboard
     Route::get('/updateEmployee-form' , function(){ return view('auth.update'); })->name('update.employee.form');
     Route::get('/update', [UserController::class, 'update'])->name('update.employee');
